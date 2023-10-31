37fe12bac7e207245ce0932b427016e70cb391d2
THARUNIKA G <95226701+Tharunika-21@users.noreply.github.com>
2023-10-31 19:08:09 +0530

    Add files via upload

--- controllers/bookController.js
+++ controllers/bookController.js
@@ -1,1 +1,66 @@
+const Book = require('../models/Book');
 
+const addBook = async (req, res) => {
+  const { title, author, summary } = req.body;
+  const newBook = new Book({ title, author, summary });
+  try {
+    const savedBook = await newBook.save();
+    res.json(savedBook);
+  } catch (err) {
+    res.status(400).json({ message: err.message });
+  }
+};
+
+const getAllBooks = async (req, res) => {
+  try {
+    const books = await Book.find();
+    res.json(books);
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
+};
+
+const getBookById = async (req, res) => {
+  try {
+    const book = await Book.findById(req.params.id);
+    if (!book) {
+      return res.status(404).json({ message: 'Book not found' });
+    }
+    res.json(book);
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
+};
+
+const updateBook = async (req, res) => {
+  try {
+    const { title, author, summary } = req.body;
+    const updatedBook = await Book.findByIdAndUpdate(req.params.id, { title, author, summary }, { new: true });
+    if (!updatedBook) {
+      return res.status(404).json({ message: 'Book not found' });
+    }
+    res.json(updatedBook);
+  } catch (err) {
+    res.status(400).json({ message: err.message });
+  }
+};
+
+const deleteBook = async (req, res) => {
+  try {
+    const deletedBook = await Book.findByIdAndRemove(req.params.id);
+    if (!deletedBook) {
+      return res.status(404).json({ message: 'Book not found' });
+    }
+    res.json({ message: 'Book deleted' });
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
+};
+
+module.exports = {
+  addBook,
+  getAllBooks,
+  getBookById,
+  updateBook,
+  deleteBook
+};
