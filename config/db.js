339797df490e3017789e122c553c604bc63a3f70
THARUNIKA G <95226701+Tharunika-21@users.noreply.github.com>
2023-10-31 19:06:15 +0530

    Update db.js

--- config/db.js
+++ config/db.js
@@ -1,1 +1,8 @@
+const mongoose = require('mongoose');
+
+// Connect to MongoDB (replace with your connection string)
+mongoose.connect('YOUR_MONGODB_CONNECTION_STRING', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+});
 
